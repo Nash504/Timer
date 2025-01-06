f4fb0a1d4cb0967cafaf06707664c8772885ca3b
Nash504 <nashdfern@gmail.com>
2025-01-06 18:55:59 +0300

    nash is gay

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
+from supabase_py import create_client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__, template_folder='templates')  # Corrected folder name
+supabase_url = os.getenv('SUPABASE_URL')
+supabase_key = os.getenv('SUPABASE_KEY')
+supabase = create_client(supabase_url, supabase_key)
+
+@app.route('/')
+def index():
+    response = supabase.table("times").select("*").execute()
+    return render_template('index.html', times=response['data'])
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    name = request.form.get('name')
+    time = request.form.get('time')
+    response = supabase.table("times").insert([{"name": name, "time": time}]).execute()
+    return redirect(url_for("index"))
+
+@app.route('/remove_user/<string:name>')
+def remove_user(name):
+    response = supabase.table("times").delete().eq("name", name).execute()
+    return redirect(url_for("index"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
